eef71b27978a95fb0ec5a1337f20461225fcdebf
zp160 <zp160@mail.ustc.edu.cn>
2017-11-06 19:02:39 +0800

    feat(tmall.filter): 添加filter拦截浏览器请求，减少Servlet部分冗余代码

---
+++ src/tmall/filter/BackServletFilter.java
@@ -0,0 +1,49 @@
+package tmall.filter;
+
+import org.apache.commons.lang.StringUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class BackServletFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain filter) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+
+        /*
+        路径：resin/webapps/my_proj/test/request.jsp
+        http://localhost:8080/my_proj/test/request.jsp?***
+
+        request.getRequestURI() /my_proj/test/request.jsp
+        request.getRequestURL() http://localhost:8080/my_proj/test/request.jsp
+        request.getContextPath() /my_proj 带/WEB-INF的目录地址
+        request.getServletPath()  /test/request.jsp
+         */
+//        String contextPath = req.getServletContext().getContextPath();//返回站点的根路径
+//        String uri = req.getRequestURI();
+//        uri = StringUtils.remove(uri, contextPath);
+        String uri = request.getServletPath();//获取浏览器请求的路径（除根目录外）
+        if (uri.startsWith("/admin")) {
+            String servletPath = StringUtils.substringBetween(uri, "_", "_") + "Servlet";
+            String method = StringUtils.substringAfterLast(uri, "_");
+            req.setAttribute("method", method);
+            req.getRequestDispatcher("/" + servletPath).forward(req, resp);
+            return;
+        }
+        filter.doFilter(request, response);
+    }
+
+    public void destroy() {
+
+    }
+
+}
---
+++ src/tmall/servlet/CategoryServlet.java
@@ -0,0 +1,12 @@
+package tmall.servlet;
+
+import tmall.util.Page;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CategoryServlet {
+    public String add(HttpServletRequest req, HttpServletResponse resp, Page page) {
+        return null;
+    }
+}
