502232ef5b8428610cd1b9cf9767545f96af2ccf
zp160 <zp160@mail.ustc.edu.cn>
2017-11-10 14:41:23 +0800

    feat(tmall.util): 添加了工具类ImageUtil转换不同图片格式到jpg

---
+++ src/tmall/util/ImageUtil.java
@@ -0,0 +1,51 @@
+package tmall.util;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.*;
+import java.io.File;
+import java.io.IOException;
+
+public class ImageUtil {
+
+    public static BufferedImage change2jpg(File f) {
+        try {
+            Image image = Toolkit.getDefaultToolkit().createImage(f.getAbsolutePath());
+            PixelGrabber pg = new PixelGrabber(image, 0, 0, -1, -1, true);
+            pg.grabPixels();
+            int width = pg.getWidth(), height = pg.getHeight();
+            final int[] RGB_MASKS = {0xFF0000, 0xFF00, 0xFF};
+            final ColorModel RGB_OPAQUE = new DirectColorModel(32, RGB_MASKS[0], RGB_MASKS[1], RGB_MASKS[2]);
+            DataBuffer dataBuffer = new DataBufferInt((int[]) pg.getPixels(), pg.getWidth() * pg.getHeight());
+            WritableRaster raster = Raster.createPackedRaster(dataBuffer, width, height, width, RGB_MASKS, null);
+            BufferedImage bufferedImage = new BufferedImage(RGB_OPAQUE, raster, false, null);
+            return bufferedImage;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void resizeImage(File srcFile, int width, int height, File destFile) {
+        try {
+            Image image = ImageIO.read(srcFile);
+            image = resizeImage(image, width, height);
+            ImageIO.write((RenderedImage) image, "jpg", destFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Image resizeImage(Image srcImage, int width, int height) {
+        try {
+            BufferedImage bufferedImage = null;
+            bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            bufferedImage.getGraphics().drawImage(srcImage.getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH), 0, 0, null);
+            return bufferedImage;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
