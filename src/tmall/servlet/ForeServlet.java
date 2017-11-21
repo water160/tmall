2d4c5ba8704822f6df6179fa84ff0cf8cccc776a
water160 <zp160@mail.ustc.edu.cn>
2017-11-21 22:54:28 +0800

    fix(tmall.servlet): 添加register方法响应客户端注册请求

--- src/tmall/servlet/ForeServlet.java
+++ src/tmall/servlet/ForeServlet.java
@@ -1,6 +1,8 @@
 package tmall.servlet;
 
+import org.springframework.web.util.HtmlUtils;
 import tmall.bean.Category;
+import tmall.bean.User;
 import tmall.dao.CategoryDAO;
 import tmall.dao.ProductDAO;
 import tmall.util.Page;
@@ -16,5 +18,23 @@ public class ForeServlet extends BaseForeServlet {
         new ProductDAO().fillByRow(c_list);//Category中的productListByRow【List<List<Product>>】被填充，一行8个
         request.setAttribute("c_list", c_list);
         return "front/home.jsp";
+    }
+
+    public String register(HttpServletRequest request, HttpServletResponse response, Page page) {
+        String name = request.getParameter("name");
+        String password = request.getParameter("password");
+        name = HtmlUtils.htmlEscape(name);//防止恶意注册，如：<script>alert('papapa')</script>弹出一个对话框
+
+        boolean exist = userDAO.isExist(name);
+        if(exist) {
+            request.setAttribute("msg", "用户名已经被使用，请重新输入用户名");
+            return "front/register.jsp";
+        }
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        userDAO.add(user);
+
+        return "@front/registerSuccess.jsp";
     }
 }
